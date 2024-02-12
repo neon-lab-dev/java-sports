319b7e757c7059cdf0823b74661c82e7829bd5a2
Tilak Thapa <tilakthapa9898@gmail.com>
2024-02-12 14:11:37 +0000

    Add accordion links

---
+++ src/assets/constants/accordion_links.js
@@ -0,0 +1,98 @@
+const ACCORDION_LINKS = [
+  {
+    label: "Appreal",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "Jerseys", link: "/" },
+      { label: "Pants", link: "/" },
+      { label: "Caps", link: "/" },
+      { label: "Jackets", link: "/" },
+      { label: "Compression wear", link: "/" },
+    ],
+  },
+  {
+    label: "Batting Gears",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "Gloves", link: "/" },
+      { label: "Leg Guards", link: "/" },
+      { label: "Inner Gloves", link: "/" },
+    ],
+  },
+  {
+    label: "Bats",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "English Willow", link: "/" },
+      { label: "Kashmir Willow", link: "/" },
+      { label: "Tennis Bat", link: "/" },
+    ],
+  },
+  {
+    label: "Balls",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "Red Leather", link: "/" },
+      { label: "White Leather", link: "/" },
+      { label: "Tennis Ball", link: "/" },
+    ],
+  },
+  {
+    label: "Wicket Keeping",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "Gloves", link: "/" },
+      { label: "Leg Guards", link: "/" },
+      { label: "Inner Gloves", link: "/" },
+    ],
+  },
+  {
+    label: "Shoes",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "Spikes", link: "/" },
+      { label: "Rubber Studs", link: "/" },
+      { label: "Running Shoes", link: "/" },
+    ],
+  },
+  {
+    label: "Helmate",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "Steel Helmates", link: "/" },
+      { label: "Titanium Helmates", link: "/" },
+      { label: "Limited Edition", link: "/" },
+    ],
+  },
+  {
+    label: "Kitbags",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "Backpacks", link: "/" },
+      { label: "Duffel Bags", link: "/" },
+      { label: "Wheelie Bags", link: "/" },
+    ],
+  },
+  {
+    label: "Protection",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "Thigh Pad", link: "/" },
+      { label: "Chest Guard", link: "/" },
+      { label: "Arm Guard", link: "/" },
+      { label: "Inner Thigh Pads", link: "/" },
+    ],
+  },
+  {
+    label: "Accessories",
+    dropdowns: [
+      { label: "All", link: "/" },
+      { label: "Grips", link: "/" },
+      { label: "Cricket Balls", link: "/" },
+      { label: "Bat Covers", link: "/" },
+      { label: "Kit Bags", link: "/" },
+    ],
+  },
+];
+
+export default ACCORDION_LINKS;

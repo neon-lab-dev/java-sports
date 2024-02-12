e688ceea4a1f41a019d3407f41d9e75358a47ec7
Tilak Thapa <tilakthapa9898@gmail.com>
2024-02-12 05:37:22 +0000

    fix: update routing configuration with fallback

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,29 +1,35 @@
-import { Suspense, lazy } from "react";
+import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ROUTES from "./routes";
+import AppLayout from "@/components/reusable/AppLayout";
 
 const RoutesContainer = () => {
-  const LoginPage = lazy(() => import('@/pages/Auth/Login'));
-  const SignupPage = lazy(() => import('@/pages/Auth/Signup'));
-  const ChangePasswordPage = lazy(() => import('@/pages/Auth/ChangePassword'));
-  const ForgotPasswordPage = lazy(() => import('@/pages/Auth/ForgotPassword'));
-  const OtpPage = lazy(() => import('@/pages/Auth/Otp'));
-  const NotFoundPage = lazy(() => import('@/pages/NotFound'));
-  const HomePage = lazy(() => import("@/pages/Home"));
-
   return (
-    <Suspense fallback={ <>Loading...</> }>
-      <Router>
-        <Routes>
-          <Route path="/login" element={ <LoginPage /> } />
-          <Route path="/signup" element={ <SignupPage /> } />
-          <Route path="/otp-verification" element={ <OtpPage /> } />
-          <Route path="/forgot-password" element={ <ForgotPasswordPage /> } />
-          <Route path="/reset-password" element={ <ChangePasswordPage /> } />
-          <Route path="/" element={ <HomePage /> } />
-          <Route path="*" element={ <NotFoundPage /> } />
-        </Routes>
-      </Router>
-    </Suspense>
+    <Router>
+      <Routes>
+        {ROUTES.map(
+          ({ component: Component, path, wrapper: Wrapper }, index) => (
+            <Route
+              key={index}
+              path={path}
+              element={
+                <AppLayout>
+                  <Suspense fallback={<div>Loading...</div>}>
+                    {Wrapper ? (
+                      <Wrapper>
+                        <Component />
+                      </Wrapper>
+                    ) : (
+                      <Component />
+                    )}
+                  </Suspense>
+                </AppLayout>
+              }
+            />
+          )
+        )}
+      </Routes>
+    </Router>
   );
 };
 export default RoutesContainer;

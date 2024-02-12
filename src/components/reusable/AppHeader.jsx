fe2e91db9435e01ce077a9aeb94dd51ed13d6989
Tilak Thapa <tilakthapa9898@gmail.com>
2024-02-12 08:52:28 +0000

    fix: Refactor AppHeader component to use dynamic social links and other links

--- src/components/reusable/AppHeader.jsx
+++ src/components/reusable/AppHeader.jsx
@@ -1,42 +1,65 @@
-import AppDesktopNavbar from './AppDesktopNavbar'
-import AppHeaderNav from './AppHeaderNav'
+import AppDesktopNavbar from "./AppDesktopNavbar";
+import AppHeaderNav from "./AppHeaderNav";
 
 // @ts-ignore
-import FaceBook from '@/assets/icons/facebook.svg'
+import Facebook from "@/assets/icons/facebook.svg";
 // @ts-ignore
-import Instagram from '@/assets/icons/instagram.svg'
+import Instagram from "@/assets/icons/instagram.svg";
+
+const HEADER_LINKS = {
+  socialLinks: [
+    {
+      link: "https://facebook.com",
+      icon: Facebook,
+      label: "Facebook",
+    },
+    {
+      link: "https://instagram.com",
+      icon: Instagram,
+      label: "Instagram",
+    },
+  ],
+  others: [
+    {
+      path: "/about",
+      label: "About Us",
+    },
+    {
+      path: "/payment-delivery",
+      label: "Payment & delivery",
+    },
+    {
+      path: "/contact",
+      label: "Contact Us",
+    },
+    {
+      path: "/help",
+      label: "Help",
+    },
+  ],
+};
 
 const AppHeader = () => {
   return (
     <>
       <header>
-        <nav className="bg-grey/1 py-4 md:block hidden">
+        <nav className="hidden py-4 bg-grey/1 md:block">
           <div className="wrapper flex justify-between items-center text-grey-dark text-[12px] font-[500] font-Jakarta">
             <ul className="flex gap-8">
-              <li>
-                <a href="">
-                  <img src={ FaceBook } alt="Facebook" />
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <img src={ Instagram } alt="Instagram" />
-                </a>
-              </li>
+              {HEADER_LINKS.socialLinks.map(({ label, link, icon }) => (
+                <li key={label}>
+                  <a href={link} target="_blank" rel="noreferrer">
+                    <img src={icon} alt={label} />
+                  </a>
+                </li>
+              ))}
             </ul>
-            <ul className="flex gap-9 capitalize">
-              <li>
-                <a href="">About us</a>
-              </li>
-              <li>
-                <a href="">Payment & delivery</a>
-              </li>
-              <li>
-                <a href="">Contact us</a>
-              </li>
-              <li>
-                <a href="">Help</a>
-              </li>
+            <ul className="flex capitalize gap-9">
+              {HEADER_LINKS.others.map(({ label, path }) => (
+                <li key={path}>
+                  <a href={path}>{label}</a>
+                </li>
+              ))}
             </ul>
           </div>
         </nav>
@@ -45,10 +68,9 @@ const AppHeader = () => {
 
         {/* DESKTOP NAVBAR */}
         <AppDesktopNavbar />
-        
       </header>
     </>
-  )
-}
+  );
+};
 
-export default AppHeader
+export default AppHeader;

74d8d93349e51f26df61d680cd4c2907c31e39c5
Tilak Thapa <tilakthapa9898@gmail.com>
2024-02-12 14:11:58 +0000

    Refactor desktop navbar component

--- src/components/reusable/AppDesktopNavbar.jsx
+++ src/components/reusable/AppDesktopNavbar.jsx
@@ -1,15 +1,20 @@
-const AppDesktopNavbar = () => {
-  const navLinks = ['Appreal', 'Batting Gears', 'bats', 'Balls', 'Wicket Keeping', 'Shoes', 'Helmate', 'Kitbags', 'Protection', 'Accessories'];
+import ACCORDION_LINKS from "@/assets/constants/accordion_links";
 
+const AppDesktopNavbar = () => {
   return (
-    <div className="mt-[20px] bg-white flex items-center">
-      <div className="wrapper flex items-center capitalize">
-        <ul className="flex flex-1 items-center h-[83px] font-Jakarta text-[1.5rem] font-500 leading-6 overflow-x-auto whitespace-nowrap gap-[4.25rem] cursor-pointer">
-          { navLinks && navLinks.map((item, index) => <li key={ index } className="pointer">{ item }</li>) }
+    <div className="mt-[20px] bg-white items-center hidden md:flex">
+      <div className="flex items-center capitalize wrapper">
+        <ul className="flex items-center flex-1 gap-20 pt-5 pb-6 overflow-x-scroll text-2xl leading-6 cursor-pointer snap-x font-Jakarta font-500 whitespace-nowrap">
+          {ACCORDION_LINKS.map(({ dropdowns, label }, index) => (
+            <li key={index} className="relative pointer snap-start group">
+              <button className="transition-all group-hover:text-primary group-hover:underline">
+                {label}
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
-  )
-}
-export default AppDesktopNavbar
-
+  );
+};
+export default AppDesktopNavbar;

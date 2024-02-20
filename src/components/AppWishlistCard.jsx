0700c2ec7fd34a6ebba24f44fe0b6aa5d366abde
MekalaArunKumar16 <mekalaarunkumar2002@gmail.com>
2024-02-20 10:01:05 +0530

    created wishlist

--- src/components/AppWishlistCard.jsx
+++ src/components/AppWishlistCard.jsx
@@ -3,7 +3,7 @@
 import close from '@assets/icons/Wishlist-close.svg'
 import bat from "@assets/images/bats-img.svg"
 const AppWishlistCard = (props) => {
-  const cardWrapper = `  w-[300px] rounded-2xl bg-white p-6 border m-5  max-lg:w-[300px]  max-sm:w-[180px]  `;
+  const cardWrapper = `  w-[300px] max-sm:w-[280px] rounded-2xl bg-white p-6 border m-3`;
   const imageWrapper = `bg-grey/1 rounded-2xl my-4 p-3 flex justify-center items-center  `
   return (
     <article className={ `${cardWrapper} card-shadow` }>
@@ -17,18 +17,18 @@ const AppWishlistCard = (props) => {
         <img src={ bat } alt={ props.title } />
       </div>
       <div className='flex flex-col gap-3'>
-        <span>{ props.title } Pro Elite Bat</span>
+        <span className='font-Lato font-700 text-xl'>{ props.title } Pro Elite Bat</span>
         <div className='flex justify-between items-center'>
           <ul className='flex gap-2 items-center'>
             <li className='font-Lato font-700 text-lg'>
-              <span>₹{ props.price } 6000</span>
+              <span>₹{ props.price } 600</span>
             </li>
             <li className='font-Lato font-500 text-sm line-through'>
-              <span>₹{ props.discounted } 43</span>
+              <span>₹{ props.discounted } 900</span>
             </li>
           </ul>
         </div>
-          <button className='bg-primary/2 text-white  p-1 rounded'>Move TO Cart</button>
+          <button className='bg-primary/2 text-white font-500 text-lg  p-1 rounded'>Move TO Cart</button>
       </div>
     </article>
   )
--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -6,56 +6,60 @@ import pervious from '@assets/icons/pre.svg'
 import next from '@assets/icons/next.svg'
 import AppProductSlider from '@/components/reusable/AppProductSlider'
 import ACCESSORIES from "@/assets/mock-data/accessories";
+import second from '@assets/mock-data/accessories'
+import { Link } from 'react-router-dom'
 
 
 
 const wishlist = () => {
     return (
         <>
-            <div className='flex flex-col gap-5'>
+            <div className='flex flex-col'>
                 <div className=''>
                     <div className=' p-3 bg-grey/1'>
                         <div className=''>
-                            <span className=' pl-[9%] max-sm:pl-0  font-Lato text-lg font-700 '>Home {`>>`} <span className='text-red-500'>Wishlist</span></span>
+                            <span className=' pl-[11%] max-sm:pl-[3%] font-Lato text-xl font-700 '>Home {`>>`} <span className='text-red-500'>Wishlist</span></span>
                         </div>
                     </div>
-                    <div className='bg-white max-sm:pl-0 pl-[9%] flex flex-col'>
-                        <div className=''>
-                            <button className='border-black bg-neutral-white border rounded-xl p-2 m-3'>
+                    <div className='bg-white pl-[11%] max-sm:pl-[3%] flex flex-col gap-3'>
+                        <div className='my-3'>
+                            <button className='border-black  bg-neutral-white border rounded-xl p-2 '>
                                 <div className='flex'>
-                                    <img src={right} alt="right-arrow" />
-                                    <span>Contiune Shopping</span>
+                                    <Link to="/">
+                                        <img src={right} alt="right-arrow" />
+                                    </Link>
+                                    <span className='max-sm:text-xs p-1'>Contiune Shopping</span>
                                 </div>
                             </button>
                         </div>
                         <div>
-                            <span className='font-700 font-Lato text-2xl m-3'>Wishlist- 5 items</span>
+                            <span className='font-700 font-Lato text-2xl py-16'>Wishlist- 5 items</span>
                         </div>
-
-                        <div className="bg-white grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 ">
-                            <AppWishlistCard />
-                            <AppWishlistCard />
-                            <AppWishlistCard />
-                            <AppWishlistCard />
-                            <AppWishlistCard />
-                        </div>
-                        <div className='bg-white my-10'>
-
+                        <div className=' bg-white flex max-md:justify-center'>
+                            <div className="bg-white grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-10 ">
+                                <AppWishlistCard />
+                                <AppWishlistCard />
+                                <AppWishlistCard />
+                                <AppWishlistCard />
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className='bg-white max-sm:pl-0 pl-[9%] flex flex-col p-5'>
-                    <div className='flex justify-between pr-[5%]'>
-                        <span className='font-700 font-Lato text-3xl'>Products you might like</span>
-                    </div>
-                    <div className=''>
-                        <AppProductSlider items={ACCESSORIES}/>
+                <div className='m-6'>
+
+                </div>
+                <div className='bg-white'>
+                    <div className=" bg-white wrapper py-10">
+                        <span className=" font-Lato pt-[44px] text-4xl font-700 ">
+                            Products you might like
+                        </span>
+                        <AppProductSlider items={ACCESSORIES} />
                     </div>
                 </div>
+                
             </div>
-
         </>
     )
 }
-
 export default wishlist
+

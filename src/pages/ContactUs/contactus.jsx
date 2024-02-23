b67d608c8ae78c2c48681c85f3e500d149909fe0
MekalaArunKumar16 <mekalaarunkumar2002@gmail.com>
2024-02-23 10:28:50 +0530

    create responsive for contactus page

--- src/pages/ContactUs/contactus.jsx
+++ src/pages/ContactUs/contactus.jsx
@@ -5,16 +5,12 @@ import email from '@/assets/icons/Email.svg'
 import loaction from '@/assets/icons/Location on.svg'
 
 const contactUs = () => {
-    const MapStyle = {
-        width: 800,
-        height: 400
-    };
     return (
         <div className=' bg-white'>
             <div className='flex justify-center p-6'>
                 <span className='text-4xl font-600'>Contact US</span>
             </div>
-            <div className='flex justify-center p-10'>
+            <div className='flex justify-center max-sm:flex-col p-10 max-sm:p-3'>
                 <div className='flex  flex-col bg-black gap-10 p-10'>
                     <span className='text-white text-2xl'>Corprate Office</span>
                     <div className='flex flex-col gap-20'>
@@ -40,7 +36,7 @@ const contactUs = () => {
                         </div>
                     </div>
                 </div>
-                <form action="" className=' bg-grey/1 py-8 px-16 flex flex-col gap-5'>
+                <form action="" className=' bg-grey/1 py-8 px-16 max-sm:px-4 flex flex-col gap-5'>
                     <span className='text-3xl font-500 '>Write to us</span>
                     <div className="flex flex-col gap-2">
                         <label htmlFor="user" className="text-sm font-700 font-Lato ">Name</label>
@@ -48,15 +44,15 @@ const contactUs = () => {
                     </div>
                     <div className="flex flex-col gap-2">
                         <label htmlFor="user" className="text-sm font-700 font-Lato ">Email</label>
-                        <input className="p-2 border-none rounded-xl" type="text" placeholder="Enter your email" id="user" />
+                        <input className="pr-20  p-2 border-none rounded-xl" type="text" placeholder="Enter your email" id="user" />
                     </div>
                     <div className="flex flex-col gap-2">
                         <label htmlFor="user" className="text-sm font-700 font-Lato ">Phone</label>
-                        <input className="p-2 border-none rounded-xl" type="text" placeholder="Enter your phone number" id="user" />
+                        <input className="p-2  border-none rounded-xl" type="text" placeholder="Enter your phone number" id="user" />
                     </div>
                     <div className="flex flex-col gap-2">
                         <label htmlFor="user" className="text-sm font-700 font-Lato ">Message</label>
-                        <input className="p-2 border-none rounded-xl w-80 pb-40 " type="text" placeholder="Write your message here" id="user" />
+                        <input className="p-2  border-none rounded-xl pb-40 " type="text" placeholder="Write your message here" id="user" />
                     </div>
                     <div className='flex justify-center'>
                         <button className='bg-black text-white px-16 py-2 rounded-3xl'> Submit</button>
@@ -67,11 +63,24 @@ const contactUs = () => {
                 <span className='text-4xl font-700 p-6'> View On Map </span>
             </div>
             <div className='flex justify-center p-10'>
-                <div>
+                <div className='max-lg:hidden'>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.4767009195157!2d85.15455507485021!3d25.589071215717933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed589d3565c8c7%3A0xf197d8a4b0688b5d!2s90%20Feet%20Rd%2C%20Patna%2C%20Bihar!5e0!3m2!1sen!2sin!4v1708607935356!5m2!1sen!2sin"
-                        width={MapStyle.width}
-                        height={MapStyle.height}
+                        height="500vh"
+                        width="800vh"
+                        frameBorder="0"
+                        style={{ border: 0 }}
+                        allowFullScreen=""
+                        aria-hidden="false"
+                        tabIndex="0"
+                        title="JavaSport"
+                    />
+                </div>
+                <div className='lg:hidden'>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.4767009195157!2d85.15455507485021!3d25.589071215717933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed589d3565c8c7%3A0xf197d8a4b0688b5d!2s90%20Feet%20Rd%2C%20Patna%2C%20Bihar!5e0!3m2!1sen!2sin!4v1708607935356!5m2!1sen!2sin"
+                        height="300"
+                        width="300"
                         frameBorder="0"
                         style={{ border: 0 }}
                         allowFullScreen=""

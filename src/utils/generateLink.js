2f57c2404fb45d23c9ac08577721606f4d33b24a
Tilak Thapa <iamjrtilak@gmail.com>
2024-02-23 07:38:36 +0545

    fix: update type param as `/category/type`

--- src/utils/generateLink.js
+++ src/utils/generateLink.js
@@ -1,7 +1,7 @@
 const generateLink = ({ category, type }) => {
   const categoryParam = category.toLowerCase().replace(" ", "-");
   const typeParam = type.toLowerCase().replace(" ", "-");
-  return `/${categoryParam}?type=${typeParam}`;
+  return `/${categoryParam}/${typeParam}`;
 };
 
 export default generateLink;

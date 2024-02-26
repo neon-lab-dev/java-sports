9c5608db0f7f2e7edead95b941bba10902d9d701
MekalaArunKumar16 <mekalaarunkumar2002@gmail.com>
2024-02-26 17:39:35 +0530

    fix:wishlist responsive  page

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -10,22 +10,12 @@ const wishlist = () => {
         <>
             <div className='flex flex-col'>
                 <div className=''>
-                    <div className='bg-white pl-[9%] max-sm:pl-[3%] max-lg:pl-[5%] flex flex-col gap-4'>
-                        <div className='py-5'>
-                            <button className='border-black  bg-neutral-white border rounded-xl p-1'>
-                                <Link to="/" className='flex items-center'>
-                                    {/* <div className='flex gap-1'> */}
-                                        <img src={ right } alt="right-arrow" />
-                                        <span className='max-sm:text-2xs p-1'>Contiune Shopping</span>
-                                    {/* </div> */}
-                                </Link>
-                            </button>
-                        </div>
+                    <div className='bg-white pl-[9%] max-sm:pl-[6%] flex flex-col gap-4 pt-4'>
                         <div>
                             <span className='font-700 font-Lato text-2xl'>Wishlist- 5 items</span>
                         </div>
-                        <div className=' bg-white flex max-lg:justify-center'>
-                            <div className="bg-white grid grid-cols-4 max-xl:grid-col s-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-5 ">
+                        <div className=' bg-white flex max-lg:justify-center pb-10'>
+                            <div className="bg-white grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-4 ">
                                 <AppWishlistCard />
                                 <AppWishlistCard />
                                 <AppWishlistCard />

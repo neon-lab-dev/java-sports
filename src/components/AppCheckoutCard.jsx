d74094061be1e7727b402a5cf263e54f3babfaed
Tilak Thapa <iamjrtilak@gmail.com>
2024-02-27 07:36:10 +0545

    Add rounded corners for Proceed to Pay button

--- src/components/AppCheckoutCard.jsx
+++ src/components/AppCheckoutCard.jsx
@@ -33,14 +33,14 @@ const AppCheckoutCard = () => {
                 <input className="pr-24 pl-3 py-1 max-sm:pr-2  border max-sm:text-xs text-xl " type="text" placeholder="Enter the Coupon code" id="user" />
                 <button className='bg-grey/1 px-16  max-sm:px-2 font-500'>Apply</button>
             </div>
-            <button className="bg-red-500 text-lg p-1 text-white  max-lg:hidden"> Proceed to Pay</button>
+            <button className="bg-red-500 text-lg p-1 text-white  max-lg:hidden rounded-[5px]"> Proceed to Pay</button>
         </div>
     </div>
     <div className='flex lg:hidden p-1'>
         <input className="pr-24 pl-3 py-1 max-sm:pr-2 border max-sm:text-xs text-xl " type="text" placeholder="Enter the Coupon code" id="user" />
         <button className='bg-grey/1 pr-24 pl-24  max-sm:pr-2  max-sm:pl-2 font-500'>Apply</button>
     </div>
-    <button className="bg-red-500 text-lg p-1 text-white lg:hidden"> Proceed to Pay</button>
+    <button className="bg-red-500 text-lg p-1 text-white lg:hidden rounded-[5px]"> Proceed to Pay</button>
 </div>
   )
 }

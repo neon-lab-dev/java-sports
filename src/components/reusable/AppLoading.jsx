54c5d57fb4d78c13f0d4782ff5b524dd8475b74b
Tilak Thapa <iamjrtilak@gmail.com>
2024-02-28 07:36:39 +0545

    Update AppLoading component height

--- src/components/reusable/AppLoading.jsx
+++ src/components/reusable/AppLoading.jsx
@@ -2,7 +2,7 @@ import { MoonLoader } from "react-spinners";
 
 const AppLoading = () => {
   return (
-    <div className="flex items-center justify-center w-full h-52">
+    <div className="flex items-center justify-center w-full h-[calc(100vh-143.18px)] lg:h-[calc(100vh-205px)]">
       <MoonLoader className="text-primary" size={30} />
     </div>
   );

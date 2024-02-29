fa9955f91abb98b76e0304b422a4601c908d7a25
Tilak Thapa <iamjrtilak@gmail.com>
2024-02-29 21:29:15 +0545

    fix: improve the protected routes mechanism

--- src/api/orders.js
+++ src/api/orders.js
@@ -20,3 +20,27 @@ export const getAllMyOrders = () => {
       });
   });
 };
+
+export const cancelOrder = (orderId) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .put(
+        `${API.cancelOrder}/${orderId}`,
+        {},
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(
+          err.response.data.message || "Something went wrong, please try again"
+        );
+      });
+  });
+};

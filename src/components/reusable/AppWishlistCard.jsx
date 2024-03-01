2f7f8a1f70f1ae52844b761e9ca9dbbf9a333cb6
Tilak Thapa <iamjrtilak@gmail.com>
2024-03-01 18:59:03 +0545

    Refactor Wishlist component and add dynamic rendering of wishlist items

--- src/components/reusable/AppWishlistCard.jsx
+++ src/components/reusable/AppWishlistCard.jsx
@@ -1,39 +1,106 @@
 // @ts-nocheck
 // @ts-ignore
-import close from '@assets/icons/Wishlist-close.svg'
-import bat from "@assets/images/bats-img.svg"
-const AppWishlistCard = (props) => {
+import { getAProduct, removeFromWishlist } from "@/api/products";
+import { calculatePercentage } from "@/utils/calculatePercentage";
+import { getLocalStorage, setLocalStorage } from "@/utils/localStorage";
+import close from "@assets/icons/Wishlist-close.svg";
+import bat from "@assets/images/bats-img.svg";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { ClipLoader } from "react-spinners";
+const AppWishlistCard = ({ productId }) => {
   const cardWrapper = `  w-[300px]  max-xl:w-[280px] rounded-2xl bg-white p-6 border `;
-  const imageWrapper = `bg-grey/1 w-[25w  0px] h-[240px] max-xl:w-[230px] rounded-2xl my-2 p-8 flex justify-center items-center  `
+  const imageWrapper = `bg-grey/1 w-[25w  0px] h-[240px] max-xl:w-[230px] rounded-2xl my-2 p-8 flex justify-center items-center  `;
+  const queryClient = useQueryClient();
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["product", productId],
+    queryFn: () => getAProduct(productId),
+  });
+
+  const handleAddToCart = () => {
+    const items = getLocalStorage("cartItems", []);
+    let updatedItems = [];
+    const isAlreadyAdded = items.find((item) => item.id === data.product._id);
+    if (isAlreadyAdded) {
+      updatedItems = items.map((item) =>
+        item.id === data.product._id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      );
+    } else {
+      updatedItems = [...items, { id: data.product._id, quantity: 1 }];
+    }
+    setLocalStorage("cartItems", updatedItems);
+    toast.success(`Added ${data.product.name} to cart!`);
+  };
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (id) => removeFromWishlist(id),
+    onSuccess: () => {
+      toast.success(`Removed ${data.product.name} from wishlist!`);
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return null;
   return (
-    <article className={ `${cardWrapper} card-shadow` }>
+    <article className={`${cardWrapper} card-shadow`}>
       <div className="flex justify-between items-center">
-        <span className='font-Lato font-700 text-xs text-grey-light'>Java Sports</span>
-        <span>
-          <img src={close} alt='Wishlist' />
+        <span className="font-Lato font-700 text-xs text-grey-light">
+          Java Sports
         </span>
+        <button
+          disabled={isPending}
+          onClick={() => {
+            mutate(data.product._id);
+          }}
+          className="cursor-pointer"
+        >
+          {isPending ? (
+            <ClipLoader size={26} color="#00B553" />
+          ) : (
+            <img src={close} alt="Wishlist" />
+          )}
+        </button>
       </div>
-      <div className={ `${imageWrapper}`}>
-        <img src={ props.img || bat } alt={ props.title } />
+      <div className={`${imageWrapper}`}>
+        <img src={data.product.images[0].url} />
       </div>
-      <div className='flex flex-col gap-1'>
-        <span className='font-Lato font-700 text-[16px]'>{ props.title } Pro Elite Bat</span>
-        <div className='flex justify-between items-center'>
-          <ul className='flex  items-center gap-2'>
-            <li className='font-Lato font-700 text-lg'>
-              <span>₹{ props.price } 600</span>
+      <div className="flex flex-col gap-1">
+        <span className="font-Lato font-700 text-[16px]">
+          {data.product.name.split(" ").slice(0, 4).join(" ")}
+        </span>
+        <div className="flex justify-between items-center">
+          <ul className="flex  items-center gap-2">
+            <li className="font-Lato font-700 text-lg">
+              <span>₹{data.product.baseprice}</span>
             </li>
-            <li className='font-Lato font-500 text-sm line-through'>
-              <span>₹{ props.discounted } 900</span>
+            <li className="font-Lato font-500 text-sm line-through">
+              <span>₹{data.product.discountedprice}</span>
             </li>
-            <li className='font-Lato font-700 text-[13px] text-green-400'>
-              <span>₹{ props.discounted } 30%</span>
+            <li className="font-Lato font-700 text-[13px] text-green-400">
+              <span>
+                {calculatePercentage(
+                  data.product.baseprice,
+                  data.product.discountedprice
+                )}
+                % off
+              </span>
             </li>
           </ul>
         </div>
-          <button className='bg-primary/2 text-white font-500 text-lg  p-1 rounded'>Move to Cart</button>
+        <button
+          onClick={handleAddToCart}
+          className="bg-primary/2 text-white font-500 text-lg  p-1 rounded"
+        >
+          Move to Cart
+        </button>
       </div>
     </article>
-  )
-}
-export default AppWishlistCard
+  );
+};
+export default AppWishlistCard;
--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,37 +1,41 @@
 // @ts-nocheck
-import AppWishlistCard from '@/components/reusable/AppWishlistCard'
-import right from '@assets/icons/right-arrow.svg'
-import AppProductSlider from '@/components/reusable/AppProductSlider'
-import ACCESSORIES from "@/assets/mock-data/accessories";
-import { Link } from 'react-router-dom'
-import AppProductsYouMightLike from '@/components/reusable/AppProductsYouMightLike';
+import AppWishlistCard from "@/components/reusable/AppWishlistCard";
+import AppProductsYouMightLike from "@/components/reusable/AppProductsYouMightLike";
+import { useSelector } from "react-redux";
 
-const wishlist = () => {
-    return (
-        <>
-            <div className='flex flex-col'>
-                <div className=''>
-                    <div className='bg-white pl-[9%] max-sm:pl-[5%] flex flex-col gap-4 pt-4'>
-                        <div>
-                            <span className='font-700 font-Lato text-2xl'>Wishlist- 5 items</span>
-                        </div>
-                        <div className=' bg-white flex max-lg:justify-center pb-10'>
-                            <div className="bg-white grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-4 ">
-                                <AppWishlistCard />
-                                <AppWishlistCard />
-                                <AppWishlistCard />
-                                <AppWishlistCard />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='m-6'>
-
-                </div>
-                <AppProductsYouMightLike/>
+const Wishlist = () => {
+  const { user } = useSelector((state) => state.user);
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="">
+          <div className="bg-white pl-[9%] max-sm:pl-[5%] flex flex-col gap-4 pt-4">
+            <div>
+              <span className="font-700 font-Lato text-2xl">
+                Wishlist- {user.wishlist.length} items
+              </span>
             </div>
-        </>
-    )
-}
-export default wishlist
-
+            <div className=" bg-white flex max-lg:justify-center pb-10">
+              {user.wishlist.length > 0 ? (
+                <div className="bg-white grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-4 ">
+                  {user.wishlist.map((item) => (
+                    <AppWishlistCard key={item._id} productId={item.product} />
+                  ))}
+                </div>
+              ) : (
+                <div className="flex justify-center items-center h-[50vh] w-full">
+                  <span className="text-2xl text-grey-light text-center ">
+                    Your wishlist is empty
+                  </span>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="m-6"></div>
+        <AppProductsYouMightLike />
+      </div>
+    </>
+  );
+};
+export default Wishlist;

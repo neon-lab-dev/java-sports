9c705d5bb7ccb740a3c54c29d5b2aeaccb22fc18
Tilak Thapa <iamjrtilak@gmail.com>
2024-03-04 14:36:14 +0545

    Add link to product details page and remove from wishlist

--- src/components/reusable/AppWishlistCard.jsx
+++ src/components/reusable/AppWishlistCard.jsx
@@ -4,10 +4,14 @@ import { getAProduct, removeFromWishlist } from "@/api/products";
 import { calculatePercentage } from "@/utils/calculatePercentage";
 import { getLocalStorage, setLocalStorage } from "@/utils/localStorage";
 import close from "@assets/icons/Wishlist-close.svg";
+import noImage from "@assets/images/no-image.jpg";
+
 import bat from "@assets/images/bats-img.svg";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
+import { splitString } from "@/utils/splitString";
 const AppWishlistCard = ({ productId }) => {
   const cardWrapper = `  w-[300px]  max-xl:w-[280px] rounded-2xl bg-white p-6 border `;
   const imageWrapper = `bg-grey/1 w-[25w  0px] h-[240px] max-xl:w-[230px] rounded-2xl my-2 p-8 flex justify-center items-center  `;
@@ -28,10 +32,27 @@ const AppWishlistCard = ({ productId }) => {
           : item
       );
     } else {
-      updatedItems = [...items, { id: data.product._id, quantity: 1 }];
+      updatedItems = [
+        ...items,
+        {
+          id: data.product._id,
+          product: data.product._id,
+          quantity: 1,
+          color: data.product.color,
+          size: splitString(data.product.size)[0],
+          name: data.product.name,
+          image: data.product.images[0].url,
+          price: data.product.discountedprice,
+          basePrice: data.product.baseprice,
+        },
+      ];
     }
     setLocalStorage("cartItems", updatedItems);
     toast.success(`Added ${data.product.name} to cart!`);
+    //remove from wishlist
+    removeFromWishlist(data.product._id).then(() => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    });
   };
 
   const { mutate, isPending } = useMutation({
@@ -67,9 +88,17 @@ const AppWishlistCard = ({ productId }) => {
           )}
         </button>
       </div>
-      <div className={`${imageWrapper}`}>
-        <img src={data.product.images[0].url} />
-      </div>
+      <Link
+        to={`/product/${data.product._id}`}
+        className={`${imageWrapper} overflow-hidden group`}
+      >
+        <img
+          src={data.product?.images[0]?.url || noImage}
+          alt={data.product.name}
+          onError={(e) => (e.currentTarget.src = noImage)} // set this image if the provided url does not work
+          className="w-full h-full object-contain object-center group-hover:scale-105 transition-all"
+        />
+      </Link>
       <div className="flex flex-col gap-1">
         <span className="font-Lato font-700 text-[16px]">
           {data.product.name.split(" ").slice(0, 4).join(" ")}

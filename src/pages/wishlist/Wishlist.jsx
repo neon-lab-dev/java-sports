884ef566a3d1c77697719660c84f45984f27090d
Tilak Thapa <iamjrtilak@gmail.com>
2024-03-05 15:21:46 +0545

    Refactor AppProductsYouMightLike and Wishlist components

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -4,6 +4,7 @@ import AppProductsYouMightLike from "@/components/reusable/AppProductsYouMightLi
 import { useSelector } from "react-redux";
 import AppEmpty from "@/components/reusable/AppEmpty";
 import forgotPassword from "@/assets/images/forgot-password.svg";
+import nothing from "@/assets/images/nothing.svg";
 
 const Wishlist = () => {
   const { user, isAuthenticated } = useSelector((state) => state.user);
@@ -18,6 +19,16 @@ const Wishlist = () => {
       />
     );
   }
+
+  if (user.wishlist.length === 0)
+    return (
+      <AppEmpty
+        text="Your wishlist is empty"
+        img={nothing}
+        btnText="Continue Shopping"
+        to="/"
+      />
+    );
   return (
     <>
       <div className="flex flex-col">
@@ -29,17 +40,11 @@ const Wishlist = () => {
               </span>
             </div>
             <div className=" bg-white flex max-lg:justify-center pb-10">
-              {user.wishlist.length > 0 ? (
+              {user.wishlist.length > 0 && (
                 <div className="bg-white grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-4 ">
                   {user.wishlist.map((item) => (
                     <AppWishlistCard key={item._id} productId={item.product} />
                   ))}
-                </div>
-              ) : (
-                <div className="flex justify-center items-center h-[50vh] w-full">
-                  <span className="text-2xl text-grey-light text-center ">
-                    Your wishlist is empty
-                  </span>
                 </div>
               )}
             </div>

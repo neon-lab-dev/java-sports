36b903779a2ef1aa877a27d35374a409b61d515a
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-01 10:14:38 +0545

    add react ga

--- src/components/reusable/AppLayout.jsx
+++ src/components/reusable/AppLayout.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from "react";
 import AppFooter from "./AppFooter";
 import AppHeader from "./AppHeader";
 import AppRestoreScroll from "./AppRestoreScroll";
+import { pageview } from "react-ga";
+import { useLocation } from "react-router-dom";
 
 /**
  * AppLayout component
  * This component is a layout wrapper for the whole app
  */
 const AppLayout = ({ children }) => {
+  const location = useLocation();
+  useEffect(() => {
+    {
+      import.meta.env.PROD &&
+        // track page view on production only
+        pageview(location.pathname + location.search);
+    }
+  }, [location.pathname, location.search]);
+
   return (
     <>
       <AppHeader />

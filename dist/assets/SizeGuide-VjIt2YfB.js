a080472c6a462dd590925356dae8d09a3d4651f7
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-16 23:22:41 +0545

    chore: some cred

--- dist/assets/SizeGuide-VjIt2YfB.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-5Ys8rys4.js";const e="/assets/head-Sk3E3njs.svg",i=()=>s.jsx("div",{className:"py-10 bg-white",children:s.jsx("img",{src:e,alt:""})});export{i as default};

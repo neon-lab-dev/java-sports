6c5bc1551e7da6704890f26b74da9a5a2fe38b2a
jrtilak <iamjrtilak@gmail.com>
2024-06-02 22:25:36 +0545

    fix: address

--- src/pages/ContactUs/contactus.jsx
+++ src/pages/ContactUs/contactus.jsx
@@ -65,13 +65,12 @@ const contactUs = () => {
               <img src={email} alt="email" />
               <span className="text-white">info@javasports.in</span>
             </div>
-            <div className="flex">
+            <div className="flex items-start max-w-[300px]">
               <img src={loaction} alt="location" />
               <p className="text-white text-[12px]">
                 Java Sports Pvt Ltd Ground & First Floor, Chak Bairiya,
-                Karanpur, Illahibagh, <br />
-                Near Illahibagh Petrol Pump, Patna Gaya Road Patna - 800007
-                Bihar
+                Karanpur, Illahibagh, Near Illahibagh Petrol Pump, Patna Gaya
+                Road Patna - 800007 Bihar
               </p>
             </div>
           </div>

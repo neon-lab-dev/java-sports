da27abc3be7d40acd30116626cd660d0818b4e00
jrtilak <iamjrtilak@gmail.com>
2024-06-03 18:10:51 +0545

    chore: update banner sizes in auth

--- src/components/reusable/AppAuthWrapper.jsx
+++ src/components/reusable/AppAuthWrapper.jsx
@@ -9,11 +9,15 @@ const AppAuthWrapper = ({ children }) => {
   return (
     <div className="flex max-sm:flex-col-reverse bg-white justify-center">
       <div className="m-4 flex  max-sm:flex-col-reverse bg-white justify-center">
-        <div className="flex bg-black max-sm:flex-col relative max-sm:mx-5">
+        <div className="flex bg-black max-sm:flex-col relative max-sm:mx-5 fmd:min-w-[300px] xl:min-w-[400px]">
           <img src={bat} alt="bat" className="sm:hidden object-contain" />
-          <img src={batLg} alt="bat" className="max-sm:hidden w-fit h-full object-contain object-top" />
+          <img
+            src={batLg}
+            alt="bat"
+            className="max-sm:hidden w-fit h-full object-contain object-top"
+          />
         </div>
-        {children}
+        <div className="md:min-w-[400px] xl:min-w-[500px]">{children}</div>
       </div>
     </div>
   );

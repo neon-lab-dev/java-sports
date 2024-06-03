d4ea79bb90b5edb90f1afbac9213180ecc39b4d1
jrtilak <iamjrtilak@gmail.com>
2024-06-03 09:13:32 +0545

    add dist

---
+++ dist/assets/SizeGuide-X0aLe3jG.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-N6u_Fj9M.js";const e="/assets/head-6cFBCv4k.svg",i=()=>s.jsx("div",{className:"py-10 bg-white",children:s.jsx("img",{src:e,alt:""})});export{i as default};
